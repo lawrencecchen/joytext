616ef3199584ffbe0f024c9f3347805f466e6b97
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2022-08-15 00:14:44 -0700

    standalone

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import "./index.css";
 import "uno.css";
 import "@unocss/reset/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
+import ReloadPrompt from "./components/ReloadPrompt/ReloadPrompt";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <ReloadPrompt />
   </React.StrictMode>
 );
